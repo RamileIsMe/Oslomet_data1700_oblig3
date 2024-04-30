88ba8d70b7f42cb42830ea0defa810a181e68c0b
ramilep <rapar9510@oslomet.no>
2024-04-30 14:19:09 +0200

    edring

--- src/main/resources/static/index.js
+++ src/main/resources/static/index.js
@@ -1,5 +1,4 @@
 function kjopBillett(){
-
     let nyFilm = $("#film").val();
     let nyAntall = $("#antall").val();
     let nyFornavn = $("#fornavn").val();
@@ -9,42 +8,42 @@ function kjopBillett(){
 
     let feil = false
     if (nyFilm === "Velg film her"){
-        $("#ffilm").html("Velg en film").css("color","blue");
+        $("#ffilm").html("Velg en film").css("color","red");
         feil = true;
     }else{
         $("#ffilm").html("");
     }
 
     if (nyAntall === "" || nyAntall === "0"){
-        $("#fantall").html("Obligatorisk utfylling*").css("color","blue");
+        $("#fantall").html("Obligatorisk felt*").css("color","red");
         feil = true;
     }else {
         $("#fantall").html("");
     }
 
     if (nyFornavn === ""){
-        $("#fnavn").html("Obligatorisk utfylling*").css("color","blue");
+        $("#fnavn").html("Obligatorisk felt*").css("color","red");
         feil = true;
     }else {
         $("#fnavn").html("");
     }
 
     if (nyEtternavn === ""){
-        $("#fetter").html("Obligatorisk utfylling *").css("color","blue");
+        $("#fetter").html("Obligatorisk felt *").css("color","red");
         feil = true;
     }else{
         $("#fetter").html("");
     }
 
     if (nyTel === "" || isNaN(nyTel)){
-        $("#fnr").html("Obligatorisk utfylling *").css("color","blue");
+        $("#fnr").html("Obligatorisk felt *").css("color","red");
         feil = true;
     } else{
         $("#fnr").html("")
     }
 
     if (nyMail === ""){
-        $("#fmail").html("Obligatorisk utfylling *").css("color","blue");
+        $("#fmail").html("Obligatorisk felt *").css("color","red");
         feil = true;
     }else{
         $("#fmail").html("")
@@ -61,7 +60,7 @@ function kjopBillett(){
 
     if (feil === false){
         $.post("/lagre", billett, function(){
-            visKvittering();
+            visKvitto();
         });
         $("#film").val("Velg film her");
         $("#antall").val("");
@@ -72,24 +71,25 @@ function kjopBillett(){
     }
 }
 
-function visKvittering(){
-    $.get("/visKvittering", function(data){
+function visKvitto(){
+    $.get("/visKvitto", function(data){
         formaterData(data);
     });
 }
 
 function formaterData(billetter){
-    let ut = "<table><tr><th>Film</th><th>Antall</th><th>Fornavn</th><th>Etternavn</th><th>Telefonnummer</th><th>E-post</th></tr>"
+    let ut = "<table><tr><th>Film</th><th>Antall</th><th>Fornavn</th><th>Etternavn</th><th>Tel</th><th>Mail</th></tr>"
     for (const billett of billetter){
         ut += "<tr><td>" + billett.film + "</td><td>" + billett.antall + "</td><td>" + billett.fornavn + "</td><td>" +
             billett.etternavn + "</td><td>" + billett.tel + "</td><td>"+ billett.mail + "</td></tr>";
     }
     ut += "</table>";
-    $("#kvittering").html(ut);
+    $("#kvitto").html(ut);
 }
 
 function slettAlle(){
     $.get("/slettAlle",function(){
-        $("#kvittering").html("Du har ingen gyldige billetter.");
+        $("#kvitto").html("Du har ingen billetter.");
     })
+
 }
